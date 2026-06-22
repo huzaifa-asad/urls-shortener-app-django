4c903122c01b78728caea22187d470fdfd31acaa
huzaifa-asad <khuzaifaasad55@gmail.com>
2026-06-22 17:09:14 +0500

    Fix Vercel static files: remove collectstatic dependency

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -168,14 +168,14 @@ STATICFILES_DIRS = [
 
 if not config('VERCEL', default=False, cast=bool):
     STATIC_ROOT = BASE_DIR / 'staticfiles'
-    STORAGES = {
-        'default': {
-            'BACKEND': 'django.core.files.storage.FileSystemStorage',
-        },
-        'staticfiles': {
-            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-        },
-    }
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.WhitenoiseStaticFilesStorage',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
